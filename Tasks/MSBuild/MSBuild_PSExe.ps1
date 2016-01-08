d86b865c995ffa389b29c7e48b29cbe0ab9c53a3
Eric Sciple <ersciple@microsoft.com>
2016-01-07 23:22:47 -0500

    .

--- Tasks/MSBuild/MSBuild_PSExe.ps1
+++ Tasks/MSBuild/MSBuild_PSExe.ps1
@@ -3,26 +3,26 @@ param([switch]$OmitDotSource)
 
 Trace-VstsEnteringInvocation $MyInvocation
 try {
-    [string]$MSBuildLocationMethod = Get-VstsInput -Name 'MSBuildLocationMethod'
-    [string]$MSBuildLocation = Get-VstsInput -Name 'MSBuildLocation'
-    [string]$MSBuildArguments = Get-VstsInput -Name 'MSBuildArguments'
-    [string]$Solution = Get-VstsInput -Name 'Solution' -Require
-    [string]$Platform = Get-VstsInput -Name 'Platform'
-    [string]$Configuration = Get-VstsInput -Name 'Configuration'
-    [bool]$Clean = Get-VstsInput -Name 'Clean' -AsBool
-    [bool]$RestoreNuGetPackages = Get-VstsInput -Name 'RestoreNuGetPackages' -AsBool
-    [bool]$LogProjectEvents = Get-VstsInput -Name 'LogProjectEvents' -AsBool
-    [string]$MSBuildVersion = Get-VstsInput -Name 'MSBuildVersion'
-    [bool]$RequireMSBuildVersion = Get-VstsInput -Name 'RequireMSBuildVersion' -AsBool
-    [string]$MSBuildArchitecture = Get-VstsInput -Name 'MSBuildArchitecture'
+    [string]$msBuildLocationMethod = Get-VstsInput -Name MSBuildLocationMethod
+    [string]$msBuildLocation = Get-VstsInput -Name MSBuildLocation
+    [string]$msBuildArguments = Get-VstsInput -Name MSBuildArguments
+    [string]$solution = Get-VstsInput -Name Solution -Require
+    [string]$platform = Get-VstsInput -Name Platform
+    [string]$configuration = Get-VstsInput -Name Configuration
+    [bool]$clean = Get-VstsInput -Name Clean -AsBool
+    [bool]$restoreNuGetPackages = Get-VstsInput -Name RestoreNuGetPackages -AsBool
+    [bool]$logProjectEvents = Get-VstsInput -Name LogProjectEvents -AsBool
+    [string]$msBuildVersion = Get-VstsInput -Name MSBuildVersion
+    [bool]$requireMSBuildVersion = Get-VstsInput -Name RequireMSBuildVersion -AsBool
+    [string]$msBuildArchitecture = Get-VstsInput -Name MSBuildArchitecture
     if (!$OmitDotSource) {
         . $PSScriptRoot\Helpers_PSExe.ps1
     }
 
-    $solutionFiles = Get-SolutionFiles -Solution $Solution
-    $MSBuildArguments = Format-MSBuildArguments -MSBuildArguments $MSBuildArguments -Platform $Platform -Configuration $Configuration
-    $MSBuildLocation = Select-MSBuildLocation -Method $MSBuildLocationMethod -Location $MSBuildLocation -Version $MSBuildVersion -RequireVersion:$RequireMSBuildVersion -Architecture $MSBuildArchitecture
-    Invoke-BuildTools -NuGetRestore:$RestoreNuGetPackages -SolutionFiles $solutionFiles -MSBuildLocation $MSBuildLocation -MSBuildArguments $MSBuildArguments -Clean:$Clean -NoTimelineLogger:(!$LogProjectEvents)
+    $solutionFiles = Get-SolutionFiles -Solution $solution
+    $msBuildArguments = Format-MSBuildArguments -MSBuildArguments $msBuildArguments -Platform $platform -Configuration $configuration
+    $msBuildLocation = Select-MSBuildLocation -Method $msBuildLocationMethod -Location $msBuildLocation -Version $msBuildVersion -RequireVersion:$requireMSBuildVersion -Architecture $msBuildArchitecture
+    Invoke-BuildTools -NuGetRestore:$restoreNuGetPackages -SolutionFiles $solutionFiles -MSBuildLocation $msBuildLocation -MSBuildArguments $msBuildArguments -Clean:$clean -NoTimelineLogger:(!$logProjectEvents)
 } finally {
     Trace-VstsLeavingInvocation $MyInvocation
 }
